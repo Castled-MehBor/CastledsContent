1207562d36f308d87a8c81e3663ef384bac9ee4d
Castled-MehBor <64966454+Castled-MehBor@users.noreply.github.com>
2020-07-22 19:21:35 -0400

    Final Projectiles Upload
    Hopefully, it ends up in the Projectiles folder and not in the mod folder itself.

---
+++ Projectiles/IchorFire.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CastledsContent.Projectiles
+{
+    public class IchorFire : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ichor Fire");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.magic = true;
+            projectile.CloneDefaults(ProjectileID.CursedFlameHostile);
+            aiType = ProjectileID.CursedFlameHostile;
+            projectile.timeLeft = 420;
+        }
+        public override void OnHitPlayer(Player target, int damage, bool crit)
+        {
+            if (Main.rand.NextBool(1))
+            {
+                target.AddBuff(BuffID.Ichor, 360, false);
+                target.AddBuff(BuffID.CursedInferno, 60, false);
+            }
+        }
+
+        public override void AI()
+        {
+            Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 55, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+        }
+
+    }
+}
---
+++ Projectiles/StickyReachP.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CastledsContent.Projectiles
+{
+    public class StickyReachP : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Grasp of the Slime King");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 32;
+            projectile.height = 32;
+            projectile.timeLeft = 40;
+            projectile.friendly = true;
+            projectile.penetrate = 999;
+            projectile.ignoreWater = true;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.NextBool(1))
+            {
+                target.AddBuff(BuffID.Slimed, 360, false);
+                target.AddBuff(BuffID.Slow, 360, false);
+            }
+        }
+
+        public override void AI()
+        {
+            Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 41, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+        }
+
+    }
+}
