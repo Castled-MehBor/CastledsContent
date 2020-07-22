22e3e81a335e2d09e0ce342694998720ddf8a59c
Castled-MehBor <64966454+Castled-MehBor@users.noreply.github.com>
2020-07-22 19:08:04 -0400

    Folder Upload Test
    Test of uploading files into a folder

---
+++ Items/BoneDweller/LockSmith.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CastledsContent.Items.BoneDweller
+{
+    public class LockSmith : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The LockSmith");
+            Tooltip.SetDefault("'No door is safe...'"
+            + "\nFires coins and piercing Stellar Keys!"
+            + "\n'May frighten ballistas'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 23;
+            item.ranged = true;
+            item.width = 52;
+            item.height = 36;
+            item.useTime = 28;
+            item.useAnimation = 28;
+            item.useStyle = 5;
+            item.noMelee = true;
+            item.knockBack = 5;
+            item.value = 10000;
+            item.rare = 4;
+            item.UseSound = SoundID.Item11;
+            item.shoot = mod.ProjectileType("StellarKeyProj");
+            item.shootSpeed = 32f;
+            item.autoReuse = true;
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-3, -4);
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ProjectileID.CopperCoin, damage, knockBack, player.whoAmI);
+            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ProjectileID.CopperCoin, damage, knockBack, player.whoAmI);
+            return true;
+        }
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            {
+                player.AddBuff(BuffID.BallistaPanic, 240);
+            }
+        }
+    }
+}
