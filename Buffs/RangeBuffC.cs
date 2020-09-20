5df82233e08615444bb3b33edce6889c0afea465
Mehrdad Bordbar <awsomemehrdad.95@gmail.com>
2020-09-20 13:54:44 -0400

    Forgot to commit these buffs

---
+++ Buffs/RangeBuffC.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CastledsContent.Buffs
+{
+    public class RangeBuffC : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Corrupt Energy");
+            Description.SetDefault("Movement speed and ranged critical strike chance is increased");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.moveSpeed = 0.15f;
+            player.rangedCrit = 15;
+        }
+    }
+}
