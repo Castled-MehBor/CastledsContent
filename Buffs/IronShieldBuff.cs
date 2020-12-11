1f5cf3ed0cf605035fe04c2480395c8453750f5c
Mehrdad Bordbar <awsomemehrdad.95@gmail.com>
2020-12-10 20:34:13 -0500

    IronShield buff

---
+++ Buffs/IronShieldBuff.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CastledsContent.Buffs
+{
+    public class IronShieldBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Iron Shield's Insurgence");
+            Description.SetDefault("The aegis' power is active");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.lifeRegen += 20;
+            player.noKnockback = true;
+        }
+    }
+}
