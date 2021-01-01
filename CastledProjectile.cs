645c9496d36d6dab8809407fbb3bb7259ec1b33b
LongNguyen-git <lucasnguyen2112@gmail.com>
2021-01-01 11:32:10 -0600

    Added GlobalProjectile class

---
+++ CastledProjectile.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CastledsContent;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace CastledsContent
+{
+    public class CastledProjectile : GlobalProjectile
+    {
+        public override bool InstancePerEntity => true;
+
+        //Lightful
+        public bool isLightful;
+
+        public override void SetDefaults(Projectile projectile)
+        {
+            isLightful = false;
+        }
+
+    }
+}
